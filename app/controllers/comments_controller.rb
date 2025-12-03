31be15e82c938180c915536131f37a4e006dd34e
f-sekiya <f.sekiya.tech@gmail.com>
2025-12-03 21:39:39 +0900

    コメント作成時にプロトタイプをセットする修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype, only: :create
 
   def create
     @comment = Comment.new(comment_params)
     @comments = @prototype.comments.includes(:user)
-  if @comment.save
-    redirect_to prototype_path(@comment.prototype)
-  else
-    @prototype = Prototype.find(comment_params[:prototype_id])
-    render "prototypes/show", status: :unprocessable_entity
-  end
+    if @comment.save
+      redirect_to prototype_path(@prototype)
+    else
+      render "prototypes/show", status: :unprocessable_entity
+    end
   end
 
   private
-  def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
